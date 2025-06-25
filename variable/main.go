003eb1dc5a390a30dcb3e20698917925d65f3607
fransalwan <fransalwan@gmail.com>
2025-06-25 14:43:01 -0700

    variable lesson

---
+++ variable/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is the main function where the program starts.
+	var name string = "Golang"
+	fmt.Println("Hello, " + name + "!")
+	// You can add your code here to call functions from the calculation package.
+	// For example, you can call Add or Multiply functions.
+}
